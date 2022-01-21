75ae16f70a3d58737b7822131a62f0efe10fc93d
AARÓN RODRÍGUEZ-BARBERO GONZÁLEZ <Aharon1377@gmail.com>
2022-01-22 00:56:47 +0100

    Wildcards done

--- Models/StandingDAO.cs
+++ Models/StandingDAO.cs
@@ -23,7 +23,7 @@ namespace NHL.Models
                 aux.Add(s);
             }
             standings = aux.ToArray();
-            foreach(Standing s in standings)
+            foreach (Standing s in standings)
             {
                 s.Division_Tiebreaker = getDivisionTiebreaker(s.ID_Team, s.ID_Season);
                 s.Conference_Tiebreaker = getConferenceTiebreaker(s.ID_Team, s.ID_Season);
@@ -31,6 +31,7 @@ namespace NHL.Models
             double[][] h2h = calculateH2H();
             int[][] pb = calculatePB();
             magicNumbersDivision = calculateMagicNumbersDivision(h2h, pb);
+            magicNumbersConference = calculateMagicNumbersConference(h2h, pb);
         }
 
         public short getConferenceTiebreaker(byte ID_Team, int ID_Season)
@@ -55,7 +56,7 @@ namespace NHL.Models
 
             byte[] teams = standings.Where(t => t.Points == team.Points && t.Division == team.Division && t.ID_Season == ID_Season).Select(t => t.ID_Team).ToArray();
 
-            if(teams.Length > 0)
+            if (teams.Length > 0)
             {
                 res = getTiebreakers(ID_Team, ID_Season, teams);
             }
@@ -67,7 +68,7 @@ namespace NHL.Models
         {
             short res = 0;
 
-            foreach(byte team in teams)
+            foreach (byte team in teams)
             {
                 res += MatchDAO.getTiebreaker(ID_Team, team, ID_Season);
             }
@@ -82,6 +83,37 @@ namespace NHL.Models
         public byte[] getDivisionTeamsID(byte division)
         {
             return getDivisionTeams(division).Select(s => s.ID_Team).ToArray();
+        }
+
+        public Standing[] getConferenceTeams(byte conference, bool prune3firsts)
+        {
+            List<Standing> teams = standings.Where(s => s.Conference == conference).OrderByDescending(s => s.Points).ThenByDescending(s => s.Matches_Left).ThenByDescending(s => s.Regulation_Wins).ThenByDescending(s => s.Regulation_Wins + s.Overtime_Wins).ThenByDescending(s => s.Regulation_Wins + s.Overtime_Wins + s.Shootout_Wins).ThenByDescending(s => s.Goal_Difference).ThenByDescending(s => s.Goals_For).ToList();
+
+            if (prune3firsts)
+            {
+                byte[] divisions = getConferenceDivisions(conference);
+
+                byte[] teamsA = getDivisionTeamsID(divisions[0]).ToArray();
+                byte[] teamsB = getDivisionTeamsID(divisions[1]).ToArray();
+
+                for (int i = 0; i < 3; i++)
+                {
+                    teams.Remove(teams.First(s => s.ID_Team == teamsA[i]));
+                    teams.Remove(teams.First(s => s.ID_Team == teamsB[i]));
+                }
+            }
+
+            return teams.ToArray();
+        }
+
+        public byte[] getConferenceDivisions(byte conference)
+        {
+            return standings.Where(s => s.Conference == conference).Select(s => s.Division).Distinct().ToArray();
+        }
+
+        public byte[] getConferenceTeamsID(byte conference, bool prune3firsts)
+        {
+            return getConferenceTeams(conference, prune3firsts).Select(s => s.ID_Team).ToArray();
         }
 
         private double[][] calculateH2H()
@@ -137,7 +169,7 @@ namespace NHL.Models
             {
                 byte[] PloosenessKey = standings.Select(s => s.ID_Team).ToArray();
                 int[] PloosenessValue = standings.Select(s => s.Matches_Left * 2).ToArray();
-                for(int i=0; i<PloosenessKey.Length; i++)
+                for (int i = 0; i < PloosenessKey.Length; i++)
                 {
                     Plooseness.Add(PloosenessKey[i], PloosenessValue[i]);
                 }
@@ -179,6 +211,56 @@ namespace NHL.Models
             }
             return res;
         }
+        private Dictionary<byte, double[]> calculateMagicNumbersConference(double[][] h2h, int[][] pb)
+        {
+            Dictionary<byte, double[]> res = new Dictionary<byte, double[]>();
 
+            Dictionary<byte, int> Plooseness = new Dictionary<byte, int>();
+
+            {
+                byte[] PloosenessKey = standings.Select(s => s.ID_Team).ToArray();
+                int[] PloosenessValue = standings.Select(s => s.Matches_Left * 2).ToArray();
+                for (int i = 0; i < PloosenessKey.Length; i++)
+                {
+                    Plooseness.Add(PloosenessKey[i], PloosenessValue[i]);
+                }
+            }
+
+            for (byte conference = 1; conference < 2; conference++)
+            {
+                byte[] teams = getConferenceTeamsID(conference, true);
+
+                for (int i = 0; i < teams.Length; i++)
+                {
+                    double[] aux = new double[teams.Length];
+                    for (int j = 0; j < teams.Length; j++)
+                    {
+                        int k = j;
+                        if (j >= i && j != teams.Length - 1)
+                        {
+                            k = j + 1;
+                            if (j != teams.Length - 1)
+                            {
+                                aux[j] = h2h[teams[i] - 1][teams[k] - 1];
+                            }
+                        }
+                        int Clooseness = Plooseness[teams[i]] + Plooseness[teams[k]];
+                        aux[j] += Plooseness[teams[k]] - pb[teams[i] - 1][teams[k] - 1];
+                        if (aux[j] > Clooseness)
+                        {
+                            aux[j] = double.MaxValue;
+                        }
+                        else if (aux[j] > Plooseness[teams[i]])
+                        {
+                            aux[j] = (aux[j] * 2 * Math.PI);
+                        }
+                    }
+                    Array.Sort(aux);
+                    Array.Reverse(aux);
+                    res.Add(teams[i], aux);
+                }
+            }
+            return res;
+        }
     }
 }
